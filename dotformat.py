4f80d66dd244bf34f0bcfdfbb96b4bb5834af175
prithika2030 <54575365+prithika2030@users.noreply.github.com>
2019-08-27 14:07:41 +0530

    Add files via upload

---
+++ dotformat.py
@@ -0,0 +1,4 @@
+name=input("\nEnter the name")
+college=input("\nEnter your college name")
+percentage=input("\nEnter your percentage")
+print("My name is{0} my college name is{2} My percentage is {1}".format(name,percentage,college))
