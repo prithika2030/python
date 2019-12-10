b686c87e031d01cd1f0672b9b79c4641078bccdd
prithika2030 <54575365+prithika2030@users.noreply.github.com>
2019-12-10 10:03:53 +0530

    Update bankapp.py

--- bankapp.py
+++ bankapp.py
@@ -5,13 +5,14 @@ class Bank_Account:
     def mail(self):
          email=input("Enter the email id:")
     def pas(self):
+        while True:
         pin=input("Enter the password:")
         if(len(pin)>=8):
             print(pin)
+            break
         else:
             print("Please enter the correct pin number")
-            exit(code)
-
+            continue
     def userlogin(self):
         name=input("Enter the  username:")
         for i in name:
@@ -19,19 +20,22 @@ class Bank_Account:
                 print("Enter the correct name:")
                 print(name)
     def mobile(self):
+        while True:
         mob=input("Enter the mobile number")
         if(len(mob)>=10):
             print(mob)
+            break
         else:
             print("Please enter the correct mobile number")
-            exit(code)
+            continue
     def accno(self):
         num=input("Enter the account number:")
         if(len(num)>=12):
             print(num)
+            break
         else:
             print("Enter the correct account number")
-            exit(code)
+            continue
     def deposit(self):
         amount = float(input("Enter amount to be Deposited: "))
         self.balance += amount
