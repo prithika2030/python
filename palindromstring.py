d29de05137a8fe6f74967d88d77edde45dd2315a
prithika2030 <54575365+prithika2030@users.noreply.github.com>
2020-02-16 19:23:07 +0530

    Create palindromstring.py

---
+++ palindromstring.py
@@ -0,0 +1,10 @@
+string="prithika"
+string2=" "
+for i in string:
+    string2=i+string2
+print(string)
+print(string2)
+if(string==string2):
+    print("palindrome")
+else:
+    print("not palindrome")
