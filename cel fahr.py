619842482ce64507b031b1c29a0e53f704869f9f
prithika2030 <54575365+prithika2030@users.noreply.github.com>
2020-03-30 11:36:58 +0530

    Create cel fahr.py

---
+++ cel fahr.py
@@ -0,0 +1,9 @@
+celsius = float(input("Enter the celsius:"))
+fahrenheit = (celsius * 1.8) + 32
+print(fahrenheit)
+
+
+
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print(celsius)
